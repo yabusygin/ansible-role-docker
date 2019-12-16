60a57abedbf397435313867f5d5e2b4fc54fc1d9
Alexey Busygin <busygin.contact@yandex.ru>
2019-12-16 23:22:24 +0300

    Test installation by running hello world container

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,16 @@
+"""Test role."""
+
 import os
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("instance")
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_hello_world(host):
+    """Run hello-world container."""
+    cmd = host.run_test(command="docker container run hello-world")
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert "Hello from Docker!" in cmd.stdout
