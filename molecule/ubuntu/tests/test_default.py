fed9e34d043bd7e45e20de9a0c58bc3021364335
Alexey Busygin <busygin.contact@yandex.ru>
2019-12-30 16:45:32 +0300

    Test docker group members in default setups

--- molecule/ubuntu/tests/test_default.py
+++ molecule/ubuntu/tests/test_default.py
@@ -1,6 +1,7 @@
 """Test role."""
 
 import os
+import re
 
 import testinfra.utils.ansible_runner
 
@@ -14,3 +15,13 @@ def test_hello_world(host):
     cmd = host.run_test(command="docker container run hello-world")
 
     assert "Hello from Docker!" in cmd.stdout
+
+
+def test_docker_group_members(host):
+    content = host.file(path="/etc/group").content_string
+    match = re.search(
+        pattern=r"^docker:x:\d+:$",
+        string=content,
+        flags=re.MULTILINE,
+    )
+    assert match
