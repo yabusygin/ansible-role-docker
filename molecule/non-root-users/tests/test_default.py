68d113bd7345d3c0f8535abb66cbfcd82fdf589d
Alexey Busygin <busygin.contact@yandex.ru>
2019-12-30 16:20:33 +0300

    Test non-root Docer user capabilities

--- molecule/non-root-users/tests/test_default.py
+++ molecule/non-root-users/tests/test_default.py
@@ -3,13 +3,11 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("instance")
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_non_root_user(host):
+    with host.sudo(user="vagrant"):
+        cmd = host.run_test(command="docker container run hello-world")
+    assert "Hello from Docker!" in cmd.stdout
