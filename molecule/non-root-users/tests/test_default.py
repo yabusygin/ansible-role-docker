1db5c819569faa4f9a320c7eee33e5c31017a66c
Alexey Busygin <busygin.contact@yandex.ru>
2019-12-30 16:37:03 +0300

    Test docker group memebers

--- molecule/non-root-users/tests/test_default.py
+++ molecule/non-root-users/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import testinfra.utils.ansible_runner
 
@@ -11,3 +12,13 @@ def test_non_root_user(host):
     with host.sudo(user="vagrant"):
         cmd = host.run_test(command="docker container run hello-world")
     assert "Hello from Docker!" in cmd.stdout
+
+
+def test_docker_group_members(host):
+    content = host.file(path="/etc/group").content_string
+    match = re.search(
+        pattern=r"^docker:x:\d+:vagrant$",
+        string=content,
+        flags=re.MULTILINE,
+    )
+    assert match
