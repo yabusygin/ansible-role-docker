2c6f354afa1d1478d006889ac5822cdd111e7436
Alexey Busygin <busygin.contact@yandex.ru>
2020-01-07 20:07:09 +0300

    Add journald logging on Debian

--- molecule/resources/tests/journald-logging/test_default.py
+++ molecule/resources/tests/journald-logging/test_default.py
@@ -1,3 +1,4 @@
+import json
 import os
 import textwrap
 
@@ -22,3 +23,30 @@ def test_daemon_config(host):
     )
     actual = host.file(path="/etc/docker/daemon.json").content_string
     assert expect == actual
+
+
+def test_journalctl(host):
+    host.run_test(
+        command="docker container run --rm --name say-hello hello-world",
+    )
+    cmd = host.run_test(
+        command="journalctl --output=json --no-pager CONTAINER_NAME=say-hello",
+    )
+    log_lines = cmd.stdout.splitlines()
+    assert len(log_lines) > 0
+
+    log_entries = [
+        json.loads(s=line)
+        for line in log_lines
+    ]
+    matches = [
+        entry
+        for entry in log_entries
+        if entry["MESSAGE"] == "Hello from Docker!"
+    ]
+    assert len(matches) > 0
+
+    log_entry = matches[-1]
+    expect_tag = "say-hello/" + log_entry["CONTAINER_ID_FULL"]
+    assert log_entry["CONTAINER_TAG"] == expect_tag
+    assert log_entry["SYSLOG_IDENTIFIER"] == expect_tag
