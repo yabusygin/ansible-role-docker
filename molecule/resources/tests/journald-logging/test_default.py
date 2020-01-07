db00318f06e1f614915c68f78faac95a4640dcec
Alexey Busygin <busygin.contact@yandex.ru>
2020-01-07 20:36:20 +0300

    Test logging of env vars to journald on Debian

--- molecule/resources/tests/journald-logging/test_default.py
+++ molecule/resources/tests/journald-logging/test_default.py
@@ -26,12 +26,22 @@ def test_daemon_config(host):
 
 
 def test_journalctl(host):
-    host.run_test(
-        command="docker container run --rm --name say-hello hello-world",
+    args = (
+        "docker", "container", "run",
+        "--rm",
+        "--name", "say-hello",
+        "--env", "TEST1=foo",
+        "--env", "TEST2=bar",
+        "hello-world",
     )
-    cmd = host.run_test(
-        command="journalctl --output=json --no-pager CONTAINER_NAME=say-hello",
+    host.run_test(command=" ".join(args))
+    args = (
+        "journalctl",
+        "--output=json",
+        "--no-pager",
+        "CONTAINER_NAME=say-hello",
     )
+    cmd = host.run_test(command=" ".join(args))
     log_lines = cmd.stdout.splitlines()
     assert len(log_lines) > 0
 
@@ -50,3 +60,5 @@ def test_journalctl(host):
     expect_tag = "say-hello/" + log_entry["CONTAINER_ID_FULL"]
     assert log_entry["CONTAINER_TAG"] == expect_tag
     assert log_entry["SYSLOG_IDENTIFIER"] == expect_tag
+    assert log_entry["TEST1"] == "foo"
+    assert log_entry["TEST2"] == "bar"
