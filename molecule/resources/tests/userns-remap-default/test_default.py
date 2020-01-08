dfc4ccc22fa2f77309aa48391117c8529e891ec1
Alexey Busygin <busygin.contact@yandex.ru>
2020-01-08 20:20:41 +0300

    Test user namespaced container on Debian

--- molecule/resources/tests/userns-remap-default/test_default.py
+++ molecule/resources/tests/userns-remap-default/test_default.py
@@ -29,7 +29,7 @@ def test_user(host):
     assert host.user(name="dockremap").exists
 
 
-def test_namespaced_directory(host):
+def test_userns_hello_world_container(host):
     entry = _get_entry(host=host, path="/etc/subuid", name="dockremap")
     assert entry is not None
 
@@ -42,6 +42,21 @@ def test_namespaced_directory(host):
 
     namespaced_dir_path = "/var/lib/docker/{}.{}".format(subuid, subgid)
     assert host.file(path=namespaced_dir_path).is_directory
+
+    args = (
+        "docker", "container", "run",
+        "--name", "say-hello",
+        "hello-world",
+    )
+    cmd = host.run_test(command=" ".join(args))
+    assert "Hello from Docker!" in cmd.stdout
+
+    matches = host.docker.get_containers(name="say-hello")
+    assert len(matches) == 1
+
+    container_id = matches[0].id
+    container_path = namespaced_dir_path + "/containers/" + container_id
+    assert host.file(path=container_path).is_directory
 
 
 def _get_entry(host, path, name):
