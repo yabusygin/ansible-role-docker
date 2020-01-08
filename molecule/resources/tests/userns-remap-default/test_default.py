4dcc4d88f0bc24acc74f36b902bf51516c7d97c0
Alexey Busygin <busygin.contact@yandex.ru>
2020-01-08 13:43:07 +0300

    Test default userns-remap config on Debian

---
+++ molecule/resources/tests/userns-remap-default/test_default.py
@@ -0,0 +1,25 @@
+import os
+import textwrap
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("instance")
+
+
+def test_daemon_config(host):
+    expect = textwrap.dedent(
+        """\
+        {
+            "userns-remap": "default",
+            "log-driver": "json-file",
+            "log-opts": {
+                "max-size": "10m",
+                "max-file": "3"
+            }
+        }
+        """
+    )
+    actual = host.file(path="/etc/docker/daemon.json").content_string
+    assert expect == actual
